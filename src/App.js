501458a5d7e9dcb09e505d7d2b30afc32050510c
Muthumeena134 <meenu842002@gmail.com>
2025-02-06 10:46:27 +0530

    to

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 // src/App.js
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Pages/Header'; // <-- Import Header here
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Pages/Header';
+import Sidebar from './Pages/Sidebar';
 import Home from './Pages/Home';
 import News from './Pages/News';
-
+import Sports from './Pages/Sports';
+import Entertainment from './Pages/Entertainment';
 
 function App() {
   return (
     <Router>
-      <Header /> {/* Now Header is recognized */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/news" element={<News />} />
-      
-      </Routes>
+      <Header />
+      <div className="main-container">
+        <Sidebar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/sports" element={<Sports />} />
+            <Route path="/entertainment" element={<Entertainment />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
